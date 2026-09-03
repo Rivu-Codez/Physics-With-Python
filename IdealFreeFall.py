b5a98e5f9c9bbcee7269aa13acb7fd4710315d48
Rivu Ghosh <168612403+Rivu-Codez@users.noreply.github.com>
2026-09-04 00:34:47 +0530

    IdealFreeFall

---
+++ IdealFreeFall.py
@@ -0,0 +1,6 @@
+'''
+In this code we analyse a freely falling body under the influence of gravity.
+We will use Euler's method to solve the differential equation of motion for a body in 
+free fall.The acceleration due to gravity is assumed to be constant, 
+and air resistance is neglected.
+'''
