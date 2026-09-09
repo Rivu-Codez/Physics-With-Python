d0182528033d8dd35646581edcb4011a06708a80
Rivu Ghosh <168612403+Rivu-Codez@users.noreply.github.com>
2026-09-09 22:28:04 +0530

    p=x-t and v-t graph

--- IdealFreeFall.py
+++ IdealFreeFall.py
@@ -7,11 +7,25 @@ and air resistance is neglected.
 
 import numpy as np
 import matplotlib.pyplot as plt
-x_0 = float(input("Enter the initial height of the body (in meters): "))
+import math
+#DEFINING VARIABLES AND CONSTANT g = 9.81 m/s^2
+t = np.linspace(0, 10, 100)
 v_0 = 0
 g = 9.81
-t_0 = 0
+x_0 = v_0*t + 0.5*g*t**2
 
-#Using Euler's method to solve the differential equation of motion
+#POSITION TIME GRAPH OF A PARTICLE UNDER FREE FALL
+plt.plot(t, x_0)
+plt.title('Ideal Free Fall')
+plt.xlabel('Time(s)')
+plt.ylabel('Position(m)')
+plt.show()
 
+#VELOCITY TIME GRAPH OF A PARTICLE UNDER FREE FALL
+v = v_0 + g*t
+plt.plot(t, v)
+plt.title('Velocity vs Time')
+plt.xlabel('Time(s)')
+plt.ylabel('Velocity(m/s)')
+plt.show()
 
