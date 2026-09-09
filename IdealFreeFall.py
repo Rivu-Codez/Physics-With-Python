24a470624f77a5e93e05d7c5011ab27fe7b25f37
Rivu Ghosh <168612403+Rivu-Codez@users.noreply.github.com>
2026-09-09 22:10:31 +0530

    Initial def of variables

--- IdealFreeFall.py
+++ IdealFreeFall.py
@@ -4,3 +4,14 @@ We will use Euler's method to solve the differential equation of motion for a bo
 free fall.The acceleration due to gravity is assumed to be constant, 
 and air resistance is neglected.
 '''
+
+import numpy as np
+import matplotlib.pyplot as plt
+x_0 = float(input("Enter the initial height of the body (in meters): "))
+v_0 = 0
+g = 9.81
+t_0 = 0
+
+#Using Euler's method to solve the differential equation of motion
+
+
